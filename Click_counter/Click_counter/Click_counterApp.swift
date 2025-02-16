f0af5c8eb302f2b53097b4c531684534e19f6291
luck <loicksauvage49@gmail.com>
2025-02-16 11:52:49 +0100

    Initial commit

---
+++ Click_counter/Click_counter/Click_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Click_counterApp.swift
+//  Click_counter
+//
+//  Created by Tessa Ichambe on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Click_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
